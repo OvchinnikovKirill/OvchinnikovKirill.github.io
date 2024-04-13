91b6bf61a21f8b91e940fd08a6a400c1aaad7548
OvchinnikovKirill <xedin666@gmail.com>
2024-04-13 14:30:50 +0300

    Fix mobile countdown

--- script.js
+++ script.js
@@ -14,21 +14,21 @@ document.addEventListener('click', function(event) {
     }
 });
 
- const targetDate = new Date("2024-07-18T15:20:00");
 
  function updateCountdown() {
- const now = new Date();
- const remainingTime = targetDate - now;
+     const targetDate = new Date("2024-07-18T15:20:00");
+     const now = new Date();
+     const remainingTime = targetDate - now;
 
- const days = Math.floor(remainingTime / (1000 * 60 * 60 * 24));
- const hours = Math.floor((remainingTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
- const minutes = Math.floor((remainingTime % (1000 * 60 * 60)) / (1000 * 60));
- const seconds = Math.floor((remainingTime % (1000 * 60)) / 1000);
+     const days = Math.floor(remainingTime / (1000 * 60 * 60 * 24));
+     const hours = Math.floor((remainingTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+     const minutes = Math.floor((remainingTime % (1000 * 60 * 60)) / (1000 * 60));
+     const seconds = Math.floor((remainingTime % (1000 * 60)) / 1000);
 
- document.getElementById("days").innerHTML = days.toString().padStart(2, "0");
- document.getElementById("hours").innerHTML = hours.toString().padStart(2, "0");
- document.getElementById("minutes").innerHTML = minutes.toString().padStart(2, "0");
- document.getElementById("seconds").innerHTML = seconds.toString().padStart(2, "0");
+     document.getElementById("days").innerHTML = days.toString().padStart(2, "0");
+     document.getElementById("hours").innerHTML = hours.toString().padStart(2, "0");
+     document.getElementById("minutes").innerHTML = minutes.toString().padStart(2, "0");
+     document.getElementById("seconds").innerHTML = seconds.toString().padStart(2, "0");
  }
 
  setInterval(updateCountdown, 1000);
